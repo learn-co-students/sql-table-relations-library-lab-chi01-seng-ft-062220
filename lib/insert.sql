f0d6261314eeed91920647a96ebddb858b23cd74
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-02 13:29:41 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO series VALUES (1, "harry potter", "1", "magic");
+INSERT INTO series VALUES (2, "lord of the rings", "2", "fantisy"); 
+
+
+INSERT INTO books VALUES (1,"harry one", 2000, "1");
+INSERT INTO books VALUES (2, "harry two", 2001, "1");
+INSERT INTO books VALUES (3, "harry three", 2002, "1");
+INSERT INTO books VALUES (4, "harry four", 2003, "1");
+INSERT INTO books VALUES (5, "hobbit", 1970, "2");
+INSERT INTO books VALUES (6, "lord of the ring one", 1980, "2");
+
+INSERT INTO characters VALUES (1, "harry", "my scare hurts", "human", "1");
+INSERT INTO characters VALUES (2, "ron", "my rat", "human", "1");
+INSERT INTO characters VALUES (3, "herminie", "I'm so smart", "human", "1");
+INSERT INTO characters VALUES (4, "froto", "must keep the ring safe", "mini human", "2");
+INSERT INTO characters VALUES (5, "samis", "evil!", "magic human", "2");
+INSERT INTO characters VALUES (6, "galum", "the prescious", "evil thing", "2");
+INSERT INTO characters VALUES (7, "orks", "work work work", "evil thing", "2");
+INSERT INTO characters VALUES (8, "elves", "arrows", "magic things", "2");
+
+
+INSERT INTO subgenres VALUES (1, "magic");
+INSERT INTO subgenres VALUES (2, "fantisy");
+
+INSERT INTO authors VALUES (1, "J.K");
+INSERT INTO authors VALUES (2, "Tolkin");
+
+INSERT INTO character_books VALUES (1, "1", "2");
+INSERT INTO character_books VALUES (2, "1", "2");
+INSERT INTO character_books VALUES (3, "2", "1");
+INSERT INTO character_books VALUES (4, "2", "1");
+INSERT INTO character_books VALUES (5, "1", "1");
+INSERT INTO character_books VALUES (6, "2", "2");
+INSERT INTO character_books VALUES (7, "2", "2");
+INSERT INTO character_books VALUES (8, "2", "1");
+INSERT INTO character_books VALUES (9, "1", "1");
+INSERT INTO character_books VALUES (10, "2", "1");
+INSERT INTO character_books VALUES (11, "1", "1");
+INSERT INTO character_books VALUES (12, "2", "1");
+INSERT INTO character_books VALUES (13, "1", "2");
+INSERT INTO character_books VALUES (14, "1", "1");
+INSERT INTO character_books VALUES (15, "2", "2");
+INSERT INTO character_books VALUES (16, "2", "1");
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,32 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+#authors(id, name);
+#subgenres(id, name);
+#series(id, title, author_id, subgenre_id);
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON author_id = authors.id JOIN subgenres ON subgenres.id = subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+#series(id, title, author_id, subgenre_id);
+#books(id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id TEXT);
+#characters(id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id TEXT); 
+  "SELECT series.title FROM series JOIN books ON series_id = series.id JOIN characters ON characters.author_id = series.author_id  WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  #characters(id, name, motto, species, author_id);
+  #character_books(id, book_id, character_id);
+
+  "SELECT characters.name, COUNT(characters.name) as book_count FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY book_count DESC"
 end
